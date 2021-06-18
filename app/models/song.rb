f3c308e2c74e0bc5de582182e45337d08fc85f32
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-06-18 15:46:08 -0400

    Archive Flatiron code.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,28 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  has_many :notes
+  belongs_to :artist
+  belongs_to :genre
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
+  def notes=(notes)
+    notes.each do |content|
+      self.notes << Note.new(content: content)
+    end
+  end
+
 end
